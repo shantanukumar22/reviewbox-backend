44aff863c2c69db93c26b1e8aa9bc1c65df2f48d
shantanukumar22 <justforworkandstudy@gmail.com>
2025-07-31 11:50:47 +0530

    cronadded

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ const app = express();
 import authRoutes from "./routes/authRoutes.js";
 import booksRoutes from "./routes/booksRoutes.js";
 import { connectDB } from "../src/lib/db.js";
-
+import job from "./lib/cron.js";
 const PORT = process.env.PORT || 3000;
+job.start();
 app.use(express.json());
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", booksRoutes);
 app.get("/", (req, res) => {
   res.json("backend working correctly");
 });
-app.use(cors());
 app.listen(PORT, () => {
   console.log(`port is running on  ${PORT}`);
   connectDB();
